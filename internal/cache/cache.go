cc6bee523e1a44abda674e2aebf1e83a8f0f3eae
Stupnikjs <106406099+Stupnikjs@users.noreply.github.com>
2026-05-10 09:05:31 +0200

    Update cache.go

--- internal/cache/cache.go
+++ internal/cache/cache.go
@@ -32,11 +32,7 @@ type Market struct {
 	Positions   []*BorrowPosition // Borrow positions sorted by HF asc
 }
 
-type MarketReader interface {
-	Ids() [][32]byte
-	GetSnapshot(id [32]byte) *MarketSnapshot
-	Update(id [32]byte, fn func(m *Market))
-}
+
 
 type Oracle struct {
 	Price   *big.Int
