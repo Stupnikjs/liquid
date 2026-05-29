ab33a7428727ff767124ebe9740c5a010ae8a55e
Stupnikjs <106406099+Stupnikjs@users.noreply.github.com>
2026-05-29 15:07:59 +0200

    Update db.go

--- internal/utils/db.go
+++ internal/utils/db.go
@@ -1,2 +1,24 @@
 package utils
 
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+
+	_ "modernc.org/sqlite"
+)
+
+func main() {
+	db, err := sql.Open("sqlite", "app.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	if err := db.Ping(); err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("sqlite opened")
+}
